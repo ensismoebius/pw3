3a153a65ae6b20f1466a4870a8f7a48e1023e836
Andre Furlan <ensismoebius@gmail.com>
2023-02-23 21:07:30 -0300

    Aula do dia 23 02 2022

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+
 require './Src/Lib/vendor/autoload.php';
-echo DB_SENHA;
+
+// Cria o roteador
+$roteador = new CoffeeCode\Router\Router(URL);
+
+// Informa o diretorio onde os controladores se encontram
+$roteador->namespace("Src\Controller");
+
+/*
+ *  Rota principal
+ */
+$roteador->group(null);
+$roteador->get("/contato/{gerente}", "Comum:entreEmContato");
+$roteador->get("/contato", "Comum:entreEmContato");
+$roteador->get("/", "Comum:principal");
+
