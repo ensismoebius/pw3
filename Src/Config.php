c9fa24f609c4484e1e2f8fed6ecefe2c4ef98fee
Andre Furlan <ensismoebius@gmail.com>
2023-03-09 18:59:29 -0300

    Exemplo de mongodb

--- Src/Config.php
+++ Src/Config.php
@@ -1,8 +1,8 @@
 <?php
-declare(strict_types = 1);
 
-define("DB_SENHA", "1234");
-define("DB_LOGIN", "root");
-define("DB_URL", "mysql:host=127.0.0.1;dbname=pw3");
+declare(strict_types=1);
+
+define("DB_BASE", "Caverna");
+define("DB_URL", "mongodb+srv://root:1234@cluster0.ojjwq5c.mongodb.net/?retryWrites=true&w=majority");
 
 define("URL", "http://localhost/pw3");
--- index.php
+++ index.php
@@ -2,28 +2,100 @@
 
 require './Src/Lib/vendor/autoload.php';
 
-// Cria o roteador
-$roteador = new CoffeeCode\Router\Router(URL);
+$serverApi = new \MongoDB\Driver\ServerApi(\MongoDB\Driver\ServerApi::V1);
+$client = new \MongoDB\Client(DB_URL, [], ['serverApi' => $serverApi]);
 
-// Informa o diretorio onde os controladores se encontram
-$roteador->namespace("Src\Controller");
+$collection = $client->{DB_BASE}->Pessoa;
 
-/*
- *  Rota principal
- */
-$roteador->group(null);
-$roteador->get("/contato/{gerente}", "Comum:entreEmContato");
-$roteador->get("/contato", "Comum:entreEmContato");
 
-$roteador->post("/contato", "Comum:salvarMensagem");
+// Gravar
+$result = $collection->insertOne([
+    'nome' => 'Joe Marie',
+    'email' => 'joe@marie.com',
+        ]);
 
-$roteador->get("/", "Comum:principal");
+echo 'Documento inserido: ' . $result->getInsertedId() . "\n";
 
-$roteador->group("admin");
-$roteador->get("/login", "Admin:exibirLogin");
-$roteador->post("/salvar", "Admin:salvarLogin");
-$roteador->get("/", "Admin:painel");
+// Encontrar
+$cursor = $collection->find([]);
 
-$roteador->dispatch();
+foreach ($cursor as $document) {
+    var_dump($document);
+}
 
-//var_dump($roteador->error());
+
+// Atualizar
+$result = $collection->updateOne(
+        ['_id' => new MongoDB\BSON\ObjectID('6142fb48a8a74121435e864d')],
+        ['$set' => ['email' => 'joe@marie.org']]
+);
+
+echo 'Modificados ' . $result->getMatchedCount() . ' documentos' . "\n";
+
+// Delete
+$result = $collection->deleteOne(['_id' => new MongoDB\BSON\ObjectID('6142fb48a8a74121435e864d')]);
+
+echo 'Apagados ' . $result->getDeletedCount() . ' documentos' . "\n";
+// With objects
+
+class Person implements \MongoDB\BSON\Persistable {
+
+    private string $id;
+    private \MongoDB\BSON\UTCDateTime $createdAt;
+    private string $name;
+
+    public function __construct(string $name) {
+        $this->id = new \MongoDB\BSON\ObjectId();
+        $this->name = $name;
+        $this->createdAt = new \MongoDB\BSON\UTCDateTime();
+    }
+
+    public function getId(): \MongoDB\BSON\ObjectId {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getCreatedAt(): \MongoDB\BSON\UTCDateTime {
+        return $this->createdAt;
+    }
+
+    public function setId(\MongoDB\BSON\ObjectId $id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName(string $name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setCreatedAt(\MongoDB\BSON\UTCDateTime $createdAt) {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    function bsonSerialize(): array {
+        return [
+            '_id' => new \MongoDB\BSON\ObjectId($this->id),
+            'name' => $this->name,
+            'createdAt' => $this->createdAt,
+        ];
+    }
+
+    function bsonUnserialize(array $data): void {
+        $this->id = $data['_id']->__toString();
+        $this->name = $data['name'];
+        $this->createdAt = $data['createdAt'];
+    }
+
+}
+
+$result = $collection->insertOne(new Person('AmendoBobo'));
+
+$pessoa = $collection->findOne(['_id' => $result->getInsertedId()]);
+
+var_dump($pessoa);
+
